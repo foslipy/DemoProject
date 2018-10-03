941f271356a229fed08e548961cdd9ac48fa4c8b
foslipy <foss.workshops@gmail.com>
2018-10-03 11:14:14 +0530

    added test.sh file

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Making foss folder"
+mkdir foss demo demo1 
+
+echo "Going to foss folder"
+cd foss
+
+echo "Creating test_file.txt"
+touch test_file.txt
+
+echo ""
+echo "Showing output of ls:"
+ls
+
+echo "sudo apt-get update"
+echo "Updating linux repos"
+
+sudo apt-get update
+
